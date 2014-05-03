a5280a2a8fe4c0854326675b056af8f212249f6f
drake7707_cp <drake7707@users.noreply.github.com>
2014-05-03 06:50:00 +0000

    - Added unnecssary diacrectics test (if OCR introduced accents and umlauds etc) - Handled numbers in hyphenation (like 98-99, 5-6 etc), those are almost always page numbers and never an error - Handled subscript and superscript so it doesn't see it as 1 word

---
+++ EpubSpellChecker/Suggestion Tests/UnnecessaryDiacriticsTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace EpubSpellChecker
+{
+    [DisplayName("Unnecessary diacritics")]
+    [Description("Tests if the word can be written without diacritics.")]
+    class UnnecessaryDiacriticsTest : ITest
+    {
+        /// <summary>
+        /// Tests if the word exists in the dictionary without accents.
+        /// If it is, it'll probably mean that the OCR introduced those accents from artifacts
+        /// </summary>
+        /// <param name="we">The word entry to test</param>
+        /// <param name="fullDictionary">The full dictionary available</param>
+        public static void Test(WordEntry we, HashSet<string> fullDictionary)
+        {
+            // check if the word contains a hyphen
+            if (we.IsUnknownWord && !we.Ignore && string.IsNullOrEmpty(we.Suggestion))
+            {
+                string withoutAccents = we.Text.RemoveDiacritics();
+
+                if (we.Text != withoutAccents && fullDictionary.Contains(withoutAccents))
+                {
+                    we.Suggestion = withoutAccents;
+                    we.UnknownType = "Unnecessary diacritics";
+                }
+            }
+        }
+
+
+    }
+}
